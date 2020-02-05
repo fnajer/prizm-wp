4d561098fb5ae8e525542008c95e61a3b17139ac
fnajer <lexactr@gmail.com>
2020-02-06 00:44:57 +0300

    setup 404 page

--- wp-content/themes/prizmbotspace/404.php
+++ wp-content/themes/prizmbotspace/404.php
@@ -10,51 +10,18 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<main class="content-wrapper">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'prizmbotspace' ); ?></h1>
-				</header><!-- .page-header -->
+	<section class="article small-container">
+		<div class="article__container">
+			<div class="page-404">
+				<h1>404</h1>
+				Sorry, page is not found!
+			</div>
+		</div>
+	</section>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'prizmbotspace' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'prizmbotspace' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$prizmbotspace_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'prizmbotspace' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$prizmbotspace_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</main> <!-- end content wrapper -->
 
 <?php
 get_footer();
